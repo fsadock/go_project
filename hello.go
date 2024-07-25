ff0ca4525e2fbffa85681bb00cb90114bce1b27a
fsadock <fsadock@gmail.com>
2024-07-25 11:31:48 -0300

    Hello World Completed

--- hello.go
+++ hello.go
@@ -2,10 +2,37 @@ package main
 
 import "fmt"
 
-func Hello(name string) string{
-	return "Hello, " + name
+const (
+	spanish = "Spanish"
+	french = "French"
+	portuguese = "Portuguese"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+	ptHelloPrefix = "Olá, "
+)
+func Hello(name, language string) string{
+	if name == ""{
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string){
+	switch language{
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case portuguese:
+		prefix = ptHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main(){
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
